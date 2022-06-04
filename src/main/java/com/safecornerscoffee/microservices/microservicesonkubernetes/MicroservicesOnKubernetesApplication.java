217085b3121b632e51cd07e586f6a52189ec6887
safecorners <safeconers.mobile@gmail.com>
2022-06-04 09:33:06 +0900

    initialize project

---
+++ src/main/java/com/safecornerscoffee/microservices/microservicesonkubernetes/MicroservicesOnKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.microservices.microservicesonkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesOnKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesOnKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecornerscoffee/microservices/microservicesonkubernetes/MicroservicesOnKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.microservices.microservicesonkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesOnKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
