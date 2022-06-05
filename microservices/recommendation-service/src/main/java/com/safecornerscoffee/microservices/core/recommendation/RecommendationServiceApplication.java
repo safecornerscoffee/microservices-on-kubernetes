fff90c76535e7c55dad617d4de2924b3999344a2
safecorners <safeconers.mobile@gmail.com>
2022-06-05 16:24:37 +0900

    implement recommendation-service

--- microservices/recommendation-service/src/main/java/com/safecornerscoffee/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/com/safecornerscoffee/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package com.safecornerscoffee.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.safecornerscoffee.microservices")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
